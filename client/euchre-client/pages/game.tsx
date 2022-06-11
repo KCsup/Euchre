7fa05bc747980af333c3ca467807214cca83a201
KCsup <kcsup05@gmail.com>
2022-06-11 17:54:52 -0400

    Switched from Node Net to socket.io server. Making react client.

---
+++ client/euchre-client/pages/game.tsx
@@ -0,0 +1,15 @@
+import {Socket} from "socket.io-client"
+
+type GameProps = {
+    socket: Socket
+}
+const Game = ({socket}: GameProps) => {
+    return (
+	<div>
+	    
+	</div>
+    ) 
+}
+
+export default Game
+
--- server/server.ts
+++ server/server.ts
@@ -1,76 +1,33 @@
-import net = require('net')
+import express = require('express')
+import { Server } from 'socket.io'
 
-class Player {
-    name: string
-    hand: string[]
-    socket: number
-
-    constructor(name: string, socket: number) {
-	this.name = name
-	this.socket = socket
-
-	this.hand = []
+const app = express()
+const server = require('http').createServer(app)
+const io = new Server(server, {
+    cors: {
+	origin: "*",
     }
-}
+})
+
 
 const port = 6942
-const server = net.createServer()
 
-const isJson = (str: string) => {
-    try {
-	JSON.parse(str)
-	return true
-    } catch(e) {
-	return false
-    }
-}
+//app.get('/', (req, res) => {
+//  res.sendFile(__dirname + '/index.html');
+//})
 
-let clients: net.Socket[] = []
-
-let turnNumber = 0
-let players: Player[] = []
-
-server.on("connection", (socket) => {
-    clients.push(socket)
-
-    console.log(`New Connection from ${socket.remoteAddress}`)
-    
-    socket.on("data", (data) => {
-	if(!isJson(data.toString())) return	
-	
-	const json = JSON.parse(data.toString())
-	if(json["data"] == undefined) return
-
-	switch(json["eventType"]) {
-	    case "join":
-		/* Join Data:
-		 * "name": string
-		 */
-
-		players.push(new Player(json["data"]["name"], clients.indexOf(socket)))
-		
-		let out = "Players:"
-
-		for(const p of players)
-		    out += `\nName: ${p.name}\n`
-		socket.write(out)
-		break
-	    case "list":
-		socket.write(players.toString())
-	    default:
-		break
-	}
+io.on("connection", (socket) => {
+    socket.on("disconnect", () => {
+	console.log("Disconnected")
     })
 
-    socket.on("close", () => {
-	const i = clients.indexOf(socket)
-	clients.splice(i, 1)
-	
-	console.log(`${socket.remoteAddress} Disconnected`)
+    socket.on("event", (res) => {
+	console.log(res)
+	io.emit("amongus", "fuck you")
     })
 })
 
 server.listen(port, () => {
-    console.log(`Server Started on ${port}`)
+    console.log(`Server Listening on ${port}`)
 })
 
