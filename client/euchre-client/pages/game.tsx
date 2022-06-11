d9b56ff75bb04e57d0776465415478b111b0f7c4
KCsup <kcsup05@gmail.com>
2022-06-11 19:51:44 -0400

    Server client communication works.

--- client/euchre-client/pages/game.tsx
+++ client/euchre-client/pages/game.tsx
@@ -1,12 +1,19 @@
-import {Socket} from "socket.io-client"
+import { io, Socket } from 'socket.io-client'
 
 type GameProps = {
     socket: Socket
+    name: string
 }
-const Game = ({socket}: GameProps) => {
+
+const Game = ({socket, name}: GameProps) => {
+    
+    socket.emit("join", {
+	name: name
+    })
+
     return (
 	<div>
-	    
+	    <h1>Game Screen</h1> 
 	</div>
     ) 
 }
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,17 @@
 import express = require('express')
 import { Server } from 'socket.io'
 
+class Player {
+    name: string
+    hand: string[]
+
+    constructor(name: string) {
+	this.name = name
+
+	this.hand = []
+    }
+}
+
 const app = express()
 const server = require('http').createServer(app)
 const io = new Server(server, {
@@ -9,16 +20,27 @@ const io = new Server(server, {
     }
 })
 
-
 const port = 6942
+
+// Game Specific
+
+let players: Player[] = []
 
 //app.get('/', (req, res) => {
 //  res.sendFile(__dirname + '/index.html');
 //})
 
 io.on("connection", (socket) => {
+    console.log("New Connection")
+
     socket.on("disconnect", () => {
 	console.log("Disconnected")
+    })
+
+    socket.on("join", (res) => {
+	players.push(new Player(res.name))
+	console.log(`Player ${res.name} Joined!`)
+	console.log(players)
     })
 
     socket.on("event", (res) => {
